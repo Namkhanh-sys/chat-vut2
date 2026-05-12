654daa7ae0d631aebc29b10af7b17540fc166872
Namkhanh-sys <namkanhkanh@gmail.com>
2026-05-12 18:58:41 +0700

    src/routes/app.index.tsx

--- src/routes/app.index.tsx
+++ src/routes/app.index.tsx
@@ -1,4 +1,3 @@
-import { createFileRoute } from '@tanstack/react-router'
 import { createFileRoute } from "@tanstack/react-router";
 import { useI18n } from "@/lib/i18n";
 import { useSidebar } from "@/hooks/use-sidebar";
